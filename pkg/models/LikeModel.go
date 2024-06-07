80312cacae0e810c3ce268a244f3499af92a4974
Dipankar <drupd17@gmail.com>
2024-06-07 20:38:44 +0530

    create models

--- pkg/models/LikeModel.go
+++ pkg/models/LikeModel.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Like struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Comment   primitive.ObjectID `bson:"comment,omitempty" json:"comment"`
+	LikedBy   primitive.ObjectID `bson:"liked_by,omitempty" json:"liked_by"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+}
--- pkg/models/UserModel.go
+++ pkg/models/UserModel.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	Id           primitive.ObjectID `bson:"_id"`
+	FirstName    *string            `json:"first_name" validate:"required,min=2,max=100"`
+	LastName     *string            `json:"last_name" validate:"required,min=2,max=100"`
+	Email        *string            `json:"email" validate:"email,required"`
+	Password     *string            `json:"password" validate:"required,min=6"`
+	Avatar       *string            `json:"avatar" validate:"required"`
+	CoverImage   *string            `json:"cover_image"`
+	Token        *string            `json:"token"`
+	RefrestToken *string            `json:"refresh_token"`
+	CreatedAt    time.Time          `json:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at"`
+	User_id      string             `json:"user_id"`
+}
